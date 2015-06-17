185a1a7018bf80c587073080f79fd31bf5d01593
Ethan Smith <ethan@onebytegone.com>
2015-06-17 18:25:34 -0400

    Added DeepThaw

---
+++ DeepThaw/storage.py
@@ -0,0 +1,46 @@
+# Copyright 2015 Ethan Smith
+
+import os, errno
+import json
+import datetime
+
+class storage(object):
+   """wrapper abstracts storing data"""
+
+   def __init__(self, basePath):
+      self.basePath = basePath
+
+   def store(self, jsonMappable, identifiers, name):
+      package = self.fetch(identifiers, name)
+      package[str(datetime.datetime.now())] = jsonMappable
+
+      string = json.dumps(package, indent=4, separators=(',', ': '))
+
+      path = self.makePath(identifiers, name)
+
+      with open(path, "w") as outfile:
+         outfile.write(string)
+
+   def fetch(self, identifiers, name):
+      with open(self.makePath(identifiers, name), "r") as infile:
+         contents = infile.read()
+         if contents == '':
+            return {}
+         return json.loads(contents)
+      return {}
+
+   def makePath(self, identifiers, name):
+      directory = self.storageDirectory(identifiers)
+      mkdir(directory)
+      return directory + '/' + name + '.json'
+
+   def storageDirectory(self, identifiers):
+      return os.path.join(self.basePath, *identifiers)
+
+def mkdir(path):
+   try:
+      os.makedirs(path)
+   except OSError as exc:
+      if exc.errno == errno.EEXIST and os.path.isdir(path):
+         pass
+      else: raise
